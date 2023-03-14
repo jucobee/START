4ee653aa7e6fcf609e2735805bba46a1b3d4ce9a
ReidLuhn <reidluhn@gmail.com>
2023-03-14 11:08:12 -0700

    Code done

--- CG_Estimation.py
+++ CG_Estimation.py
@@ -50,9 +50,9 @@ Wengine = 2.575 * (((gas_turb + EM)/2)**0.922) * 2 # TOTAL ENGINE WEIGHT
 Sw = 826.134   # Wing area
 Wfw = Wfuel  # Weight of fuel in wing
 ARw = 16.22  # Aspect ratio of wing
-Gammaw = 5   # Wing sweep angle
+Gammaw = 5 * (np.pi / 180)   # Wing sweep angle
 lambdaw = 0.4 # Wing taper ratio
-tcw =     # Thickness to chord ratio of wing root
+tcw = 0.16    # Thickness to chord ratio of wing root
 Nz = 2.458 * 1.5    # Ultimate load factor; 1.5 x limit load factor
 Wdg = MTOW  # Design gross weight
 q = (0.0343 * (590.733 ** 2)) / 2   # Dynamic pressure at cruise
@@ -60,24 +60,25 @@ q = (0.0343 * (590.733 ** 2)) / 2   # Dynamic pressure at cruise
 Wwing = 0.036 * (Sw**0.758) * (Wfw**0.0035) * ((ARw / (np.cos(Gammaw))**2)**0.6) * (q**0.006) * (lambdaw**0.04) * (((100 * tcw) / np.cos(Gammaw))**-0.3) * ((Nz * Wdg)**0.49) * 0.82
 
 Sht = 94.19  # Area of horizantle tail
-Gammaht = 16  # Sweep angle of horizontal tail
+Gammaht = 16 * (np.pi / 180)  # Sweep angle of horizontal tail
 lambdah = 0.4  # Taper ratio of horizontal tail
-tch =       # Thickness to chord ratio of horizontal tail root
+tch = 0.14      # Thickness to chord ratio of horizontal tail root
 ARh = 4.4      # Aspect ratio of horizontal tail
-Wht = (0.016 * (Nz * Wdg)**0.414) * (q**0.168) * (Sht**0.896) * (((100 * tch) / np.cos(Gammaht))**-0.12) * (((ARh) / np.cos(Gammaht)**2)**0.043) * (lambdah**-0.02) * 0.83
+print((0.016 * ((Nz * Wdg)**0.414)) * (q**0.168) * (Sht**0.896)* (((100 * tch) / np.cos(Gammaht))**-0.12))
+Wht = (0.016 * ((Nz * Wdg)**0.414)) * (q**0.168) * (Sht**0.896) * (((100 * tch) / (np.cos(Gammaht)))**-0.12) * (((ARh) / (np.cos(Gammaht)**2))**0.043) * (lambdah**-0.02) * 0.83
 
 Svt = 146.09  # Area of vertical tail
-Gammavt = 30  # Sweep angle of vertical tail
+Gammavt = 30 * (np.pi / 180)  # Sweep angle of vertical tail
 lambdavt = 0.8 # Taper ratio of vertical tail
-tcv =   # Thickness to chord ratio of vertical tail root
+tcv = 0.14  # Thickness to chord ratio of vertical tail root
 ARvt = 1.127 # Aspect ratio of vertical tail
 Wvt = 0.073 * (1 + 0.2) * ((Nz * Wdg)**0.376) * (q**0.122) * (Svt**0.873) * (((100 * tcv) / np.cos(Gammavt))**-0.49) * (((ARvt) / np.cos(Gammavt)**2)**0.357) * (lambdavt**0.039) * 0.83
 
-Lt =    # Tail length; wing quarter-MAC to tail quarter-MAC, ft
+Lt = 31.71   # Tail length; wing quarter-MAC to tail quarter-MAC, ft
 L = 79   # Fuselage structural length, ft
-D =     # Fuselage structural depth, ft
-Sf =    # Fuselage wetted area, ft^2
-Vpr =   # Volume of pressurized area
+D = 9    # Fuselage structural depth, ft
+Sf = 1775.85   # Fuselage wetted area, ft^2
+Vpr = 3919.81   # Volume of pressurized area
 Wpress = 11.9 + (Vpr * 8)**0.271
 
 Wfusel = ((0.052 * (Sf**1.086) * ((Nz * Wdg)**0.177) * (Lt**-0.051) * ((L/D)**-0.072) * (q**0.241)) + Wpress) * 0.9
@@ -94,16 +95,16 @@ W_noselanding = 0.125 * (Nl * Wl)**0.566 * (Ln / 12)**0.845 * 0.95
 # Xcg values in feet measured from leading edge of wing
 wingXcg = 5.41 * 0.3
 fuelXcg = 5.41 * 0.2
-passengersXcg = 
-crewXcg = 
-baggageXcg = 
-batteryXcg = 
-engineXcg = 
-horizontalXcg = 
-verticalXcg = 
-noselandingXcg = 
-mainlandingXcg = 
-fuselageXcg = 
+passengersXcg = 0.5 * 5.41
+crewXcg = -22.25
+baggageXcg = -2
+batteryXcg = 10
+engineXcg = 5.175
+horizontalXcg = 44.59
+verticalXcg = 33.3375
+noselandingXcg = -20
+mainlandingXcg = 4.283
+fuselageXcg = -1.19
 
 
 sum1 = (Wwing * wingXcg) + (Wfuel * fuelXcg) + (Wpass * passengersXcg) + (Wcrew * crewXcg) + (Wbaggage * baggageXcg) + (Wbattery * batteryXcg) + (Wengine * engineXcg) + (Wht * horizontalXcg) + (Wvt * verticalXcg) + (W_noselanding * noselandingXcg) + (W_mainlanding * mainlandingXcg) + (Wfusel * fuselageXcg)
