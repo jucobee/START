a80ec39656298d636013f280afcbf071b9773282
AaronHirotani <124412042+AaronHirotani@users.noreply.github.com>
2023-04-14 23:27:05 -0700

    Add files via upload

--- B1/Wheely_Tire_ed.py
+++ B1/Wheely_Tire_ed.py
@@ -1,18 +1,17 @@
-MTOW = 55029.81867528567
-
+MTOW = 53438
 Vstall = 237.981
 
 glavity = 32.17
 
-B = 32.25 #distance between landing gears
+B = 28.25 #distance between landing gears
 H = 9.87 #CG height from ground flat tires
 
 Nf = 23.455 #foreward CG
 Na =  23.565#Aft CG location from front wheel
 
-Mf = 8.8 #fwd cg loc from rear wheel
+Mf = 4.8 #fwd cg loc from rear wheel
 
-Ma = 8.69 #aft cg loc from rear wheel
+Ma = 4.69 #aft cg loc from rear wheel
 
 NOSEnum = 2
 
@@ -60,4 +59,6 @@ print('Mf/B', Mf/B)
 KEblake = (0.5)*MTOW*(Vstall**2)/32.17
 
 print('Kinetic Energy Braking',KEblake)
+
+
 
