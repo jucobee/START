5a3b9252e8182e7ea99ecd2f2febc12e69bbe498
ReidLuhn <reidluhn@gmail.com>
2023-04-21 10:52:54 -0700

    pushing

---
+++ B2/Drag_Build_Up.py
@@ -0,0 +1,21 @@
+# Reference Lengths for Cf
+fuselage_L = 
+nacelle_L = 
+wing_L = 
+h_tail_L = 
+v_tail_L = 
+
+# Max thickness of wings and tails
+tc_wing = 
+tc_h_tail = 
+tc_v_tail = 
+
+# Chordwise location of max thickness
+xc_wing = 
+xc_h_tail = 
+xc_v_tail = 
+
+# Sweep angle of wings and tails
+gammaW = 
+gammaH = 
+gammaV = 
