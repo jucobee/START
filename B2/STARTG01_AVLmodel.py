e086fdad50597afdd776777bea6b32cba4884041
Carlos Anthony Natividad <cnatividad@ucdavis.edu>
2023-04-27 13:46:13 -0700

    increased flap coverage

--- B2/STARTG01_AVLmodel.py
+++ B2/STARTG01_AVLmodel.py
@@ -27,8 +27,6 @@ def STARTG01_AVL(phase=1,alpha=0,T=1):
     def runalpha(alpha):
         AVLsp.addInput('A A {}'.format(alpha))
 
-
-
     # setup
     AVLsp = pyAVL.AVL()
     
@@ -69,37 +67,73 @@ def STARTG01_AVL(phase=1,alpha=0,T=1):
 
 
 if __name__ == "__main__":
-    STARTG01_AVL(1,0)
-    # planeName = 'STARTG01'
-    # AVLsp = pyAVL.AVL()
-    # AVLsp.loadPlane(planeName)
-    # AVLsp.loadMass(planeName)
+    # STARTG01_AVL(1,0)
+    planeName = 'STARTG01'
+    AVLsp = pyAVL.AVL()
+    AVLsp.loadPlane(planeName)
+    AVLsp.loadMass(planeName)
 
-    # AVLsp.setAtmosphere(25e3,0)
-    # AVLsp.setVelocity(275*1.6878099)
-    # AVLsp.addInput('oper')
-    # AVLsp.addInput('C1\n')
-    # AVLsp.addInput('D5\nPM\n0')
+    AVLsp.setAtmosphere(25e3,0)
+    AVLsp.setVelocity(275*1.6878099)
+    AVLsp.addInput('oper')
+    AVLsp.addInput('C1\n')
+    AVLsp.addInput('D5\nPM\n0')
+    AVLsp.addInput('X')
+    AVLsp.addInput('ST\n')
+    AVLsp.saveOutput('ST','cruise_stab')
+
+    # Trimmed for cruise at 25kft, 275 ktas
+    # delevator = -2.712107452329664E+00
+    # dCM/da: -2.472052986715643E+00
+    # dCN/db: 9.858979130699302E-02
+    # dCl/db: -6.103720339649699E-02
+
+
+    AVLsp.addInput('oper')
+    AVLsp.addInput('C1\n')
+    # AVLsp.addInput('D1 D1 0')
+    AVLsp.addInput('A C 2.2')
+    AVLsp.addInput('D2 D2 10')
+    # AVLsp.addInput('D3 D3 0')
+    AVLsp.addInput('D4 D4 10')
+    # AVLsp.addInput('D5 PM 0')
+    AVLsp.addInput('X')
+    AVLsp.addInput('ST\n')
+    AVLsp.saveOutput('ST','takeoff_stab')
+
+    # Trimmed for CL=2.2, flaps 15 deg
+    # delevator = -2.387550832246752E+01
+    # dCM/da: -2.532687610965178E+00
+    # dCN/db: 1.108476910278335E-01 
+    # dCl/db: -1.051719944520866E-01
+
+    AVLsp.addInput('oper')
+    AVLsp.addInput('C1\n')
+    # AVLsp.addInput('D1 D1 0')
+    AVLsp.addInput('A C 3.3')
+    AVLsp.addInput('D2 D2 30')
+    # AVLsp.addInput('D3 D3 0')
+    AVLsp.addInput('D4 D4 30')
+    # AVLsp.addInput('D5 PM 0')
+    AVLsp.addInput('X')
+    AVLsp.addInput('ST\n')
+    AVLsp.saveOutput('ST','landing_stab')
+
+    # Trimmed for CL=3.3, flaps 45 deg
+    # delevator = -2.922187155599264E+01
+    # dCM/da: -2.474091604088925E+00
+    # dCN/db: 1.283649711479978E-01
+    # dCl/db: -1.428365524939934E-01 
+
+
     # AVLsp.addInput('X')
-    # AVLsp.saveOutput('FT','cruise')
 
-    # AVLsp.addInput('oper')
-    # AVLsp.addInput('C1\n')
-    # # AVLsp.addInput('D1 D1 0')
-    # AVLsp.addInput('A C 3.3')
-    # AVLsp.addInput('D2 D2 45')
-    # # AVLsp.addInput('D3 D3 0')
-    # AVLsp.addInput('D4 D4 45')
-    # # AVLsp.addInput('D5 PM 0')
-    # AVLsp.addInput('X')
-    # AVLsp.saveOutput('FT','landing')
-    
-
-    
-    # AVLsp.addInput('X')
-
-    # AVLsp.runAVL()
-    # out = AVLsp.readFT()
+    AVLsp.runAVL()
+    # out = AVLsp.readFT('cruise.out')
+    # print(out['CLtot'],out['CDind'])
+    # out = AVLsp.readFT('takeoff.out')
+    # print(out['CLtot'],out['CDind'])
+    # out = AVLsp.readFT('landing.out')
     # print(out['CLtot'],out['CDind'])
     # # post
 
