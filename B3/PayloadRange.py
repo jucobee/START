b052f85e6146d2c0f306fa3afecfb4df1b91a17c
ReidLuhn <reidluhn@gmail.com>
2023-05-05 21:33:56 -0700

    First plot attempt

--- B3/PayloadRange.py
+++ B3/PayloadRange.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 
 # 53437.91767045 53437.80789773 51823.40860772 49048.624581
@@ -14,15 +15,55 @@ wpass =  200 + 40# Weight per passenger
 
 
 MPW = MZFW - OEW    # Max Payload Weight
-MFW = 3420.54  # Max fuel weight
+MPW2 = 21428.57
+MFW = 4972  # Max fuel weight
 PPF = MTOW - OEW    # Payload + Fuel
 
+payloads = [0, 0, 0, 0]
+ranges = [0, 0, 0, 0]
 
-### MISSION BREAKDOWN ###
+## Harmonic Range
+FW = MTOW - OEW - 13000
+NCF = 2198
+HDF = 0.805 * 2198
+print(FW)
+MFW2 = (1 - 0.05) * (FW - HDF) 
+CFW = MFW2 - 2198
 TCD = 1000  # Total cruise distance (nm)
 TCF = 2774.78  # Total cruise fuel (lb)
 CSR = TCD / TCF     # Cruise Specific Range (nm/lb)
+TCD = CFW * CSR
 NCR = 234      # Non-cruise Range
-NCF = 126.9      # Non-cruise fuel
+TR = TCD + NCR  # Total range
+payloads[0] = MPW2
+payloads[1] = MPW2
+ranges[1] = TR
+
+# Fuel Range #
+FW = MTOW - OEW - 5500
+MFW = 0.95 * (5500 - HDF)
+CFW = MFW - NCF
+CR = CFW * CSR
+TR = CR + NCR
+payloads[2] = FW
+ranges[2] = TR
+
+# Ferry #
+CFW = 5500 - NCF
+CR = CFW * CSR
+TR = CR + NCR
+ranges[3] = TR
+print(payloads)
+print(ranges)
+
+plt.figure()
+plt.plot(ranges,payloads)
+plt.xlabel('Range (nmi)')
+plt.ylabel('Payload (lbs)')
+plt.title('Payload Range')
+plt.show()
+
+  
+
 
 
